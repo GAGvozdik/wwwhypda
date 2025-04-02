5481bec8d85cac914e65e123f2b013c89328b0ab
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2025-04-02 21:23:33 +0200

    fix docs

--- wwwhypda-frontend/src/components/pages/documents.tsx
+++ wwwhypda-frontend/src/components/pages/documents.tsx
@@ -12,9 +12,10 @@ import {useEffect, useState, useRef} from 'react';
 const Documents: React.FC = () => {
 
     return (
-        <>
+        <div className={styles.treeText}>
+            <h2>Documents</h2>
             {/* <SearchResultsTable/> */}
-        </>
+        </div>
     );
 };
 
