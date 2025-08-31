61efb3ca474ea11d787e4542f3505670c63f3c78
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2025-08-31 14:45:21 +0500

    fix csp

--- wwwhypda-frontend/src/components/api.tsx
+++ wwwhypda-frontend/src/components/api.tsx
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-const isDevMode = process.env.DEV_MODE === "true";
-
-const getBaseURL = () => {
-  if (isDevMode) {
-    return process.env.REACT_APP_BASE_URL_DOCKER;
-  }
-  return '/api';
-};
-
 const api = axios.create({
   baseURL: '/api',
 });
